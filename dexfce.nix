f0acf27920806f69143b8a4e852922f1cce321f8
Daria Silent <s4tch3lyy@proton.me>
2026-06-03 11:14:39 +0200

    Added edits to xfce
    Added a dock to xfce and set its settings (no declaratively configured pinned apps, not possible right now as far as i know)

---
+++ dexfce.nix
@@ -0,0 +1,11 @@
+{ config, pkgs, ... }:
+{
+	environment.systemPackages = with pkgs; [
+		xfce4-whiskermenu-plugin
+		xfce4-pulseaudio-plugin
+		blueman
+		xfce4-dockbarx-plugin
+	];
+
+	services.xserver.desktopManager.xfce.enable = true;
+}
