cff0393fef092aa2fb57ee2535e7104fcc72b6ec
Daria Silent <s4tch3lyy@proton.me>
2026-06-05 22:10:36 +0200

    Remove unused shell aliases from bash.nix
    Removed several shell aliases for editing configuration files.

--- modules/personal/bash.nix
+++ modules/personal/bash.nix
@@ -6,15 +6,11 @@
 		shellAliases = {
 			btw = "echo 'i use NixOS btw' && hyfetch";
 			nrs = "sudo nixos-rebuild switch";
+			nfu = "sudo nixos flake update --flake /etc/nixos/";
+			upgrade = "sudo nixos flake update --flake /etc/nixos/ && sudo nixos-rebuild switch"
 			nixedit = "sudo vim /etc/nixos/configuration.nix";
 			flakeedit = "sudo vim /etc/nixos/flake.nix";
 			homeedit = "sudo vim /etc/nixos/home.nix";
-			plasmaedit = "sudo vim /etc/nixos/modules/plasma.nix";
-			bashedit = "sudo vim /etc/nixos/modules/bash.nix";
-			extraedit = "sudo vim /etc/nixos/modules/extra.nix";
-			useredit = "sudo vim /etc/nixos/modules/user.nix";
-			termedit = "sudo vim /etc/nixos/modules/alacritty.nix";
-			ffedit = "sudo vim /etc/nixos/modules/firefox.nix";
 		};
 		initExtra = ''
 			PS1='\[\e[92m\]\u\[\e[0m\] \[\e[96m\]@\[\e[0m\] \[\e[92m\]\H\[\e[0m\] in \[\e[96m\]\w\[\e[0m\] \\$ '
