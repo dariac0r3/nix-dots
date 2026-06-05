5dfd96e0f6b5e90d1b6f5a6fa9926333356e84b1
Daria Silent <s4tch3lyy@proton.me>
2026-06-05 19:50:40 +0200

    niri config update

--- modules/desktop/niri.nix
+++ modules/desktop/niri.nix
@@ -1,5 +1,16 @@
 { config, pkgs, ... }:
+
 {
 	programs.niri.enable = true;
 	services.xserver.enable = true;
+
+	environment.systemPackages = with pkgs; [
+		wl-clipboard
+		wayland-utils
+		cage
+		gamescope
+		nautilus
+		catppuccin-cursors.mochaMauve
+	];
+	systemd.user.services.niri.enableDefaultPath = false;
 }
