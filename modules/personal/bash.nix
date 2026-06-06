c50ee8d9b6003834a5ffe96f23c5668c5343d1c4
Daria Silent <s4tch3lyy@proton.me>
2026-06-06 15:24:20 +0200

    Update Nix commands in bash.nix aliases

--- modules/personal/bash.nix
+++ modules/personal/bash.nix
@@ -6,8 +6,8 @@
 		shellAliases = {
 			btw = "echo 'i use NixOS btw' && hyfetch";
 			nrs = "sudo nixos-rebuild switch";
-			nfu = "sudo nixos flake update --flake /etc/nixos/";
-			upgrade = "sudo nixos flake update --flake /etc/nixos/ && sudo nixos-rebuild switch";
+			nfu = "sudo nix flake update --flake /etc/nixos/";
+			upgrade = "sudo nix flake update --flake /etc/nixos/ && sudo nixos-rebuild switch";
 			nixedit = "sudo vim /etc/nixos/configuration.nix";
 			flakeedit = "sudo vim /etc/nixos/flake.nix";
 			homeedit = "sudo vim /etc/nixos/home.nix";
